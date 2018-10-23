c2fdb2c1888a2fdbe496698c0e7b36f4fad5be1d
Leon <yanglyu.leon.7@gmail.com>
2018-10-23 11:37:31 -0700

    start: Creating a new ASPNETCore project
    demo: Creating a root controller
    
    demo: Updating the media type
    
    demo: Adding versioning support

---
+++ TutorialLandonAPI/Controllers/RootController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TutorialLandonAPI.Controllers
+{
+    [Route("/")]
+    [ApiVersion("1.0")]
+    public class RootController : Controller
+    {
+        [HttpGet(Name = nameof(GetRoot))]
+        public IActionResult GetRoot()
+        {
+            var response = new
+            {
+                href = Url.Link(nameof(GetRoot), null)
+            };
+
+            return Ok(response);
+        }
+    }
+}
