73dfc5b9fc1d6f7a6ae60c7a7e677afb18267bd7
Leon <yanglyu.leon.7@gmail.com>
2018-10-23 15:02:26 -0700

    demo: Returning data from a controller
    module 4.2

---
+++ TutorialLandonAPI/Controllers/InfoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using TutorialLandonAPI.Models;
+
+namespace TutorialLandonAPI.Controllers
+{
+    [Route("/[controller]")]
+    public class InfoController:Controller
+    {
+        private readonly HotelInfo _hotelInfo;
+
+        public InfoController(IOptions<HotelInfo> hotelInfoAccessor)
+        {
+            _hotelInfo = hotelInfoAccessor.Value;
+        }
+
+        [HttpGet(Name = nameof(GetInfo))]
+        public IActionResult GetInfo()
+        {
+            _hotelInfo.Href = Url.Link(nameof(GetInfo), null);
+
+            return Ok(_hotelInfo);
+        }
+    }
+}
--- TutorialLandonAPI/Controllers/RootController.cs
+++ TutorialLandonAPI/Controllers/RootController.cs
@@ -14,6 +14,9 @@ namespace TutorialLandonAPI.Controllers
                 href = Url.Link(nameof(GetRoot), null),
                 rooms = new {
                     href = Url.Link(nameof(RoomsController.GetRooms), null)
+                },
+                info = new {
+                    href = Url.Link(nameof(InfoController.GetInfo), null)
                 }
             };
 
